7710070ffba582c7f62493d172938fff630d6912
Felipe Augusto Rosa <flp.far@gmail.com>
2021-04-02 15:34:18 -0300

    Refactor About page content

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -10,10 +10,10 @@ export default function About() {
       <Styled.LeftSection>
         <h1>
           <span className="item-square" />
-          Quem Somos
+          Somos KRAZ
         </h1>
         <p>
-          Somos Amigos, somos <strong>sócios</strong>, somos
+          Somos <strong>amigos</strong>, somos <strong>sócios</strong>, somos
           <strong> colaboradores</strong>, somos <strong>realizadores</strong>.
           Somos uma família de famintos por experiências de sucesso.
         </p>
@@ -21,22 +21,25 @@ export default function About() {
         <p>
           Com nossas próprias experiências vividas em outras empresas,
           descobrimos algumas lacunas no mercado que fazem muita diferença no
-          desempenho de uma campanha, serviço ou produto. Essas lacunas envolvem
-          problemas de comunicação, falta de atenção e foco em estratégias que
-          não geram resultados. Assim, unimos nossos conhecimentos, vivências e
-          bagagens culturais, e demos vida à <strong>KRAZ</strong>.
+          desempenho de uma campanha, serviço ou produto. Lacunas que estão
+          diretamente relacionadas à problemas de comunicação, falta de atenção,
+          e foco em estratégias que não geram resultados. Assim, unimos nossos
+          conhecimentos, vivências e bagagens culturais, e demos vida à
+          <strong> KRAZ</strong>.
         </p>
         <br />
         <p>
           <strong>KRAZ</strong> é uma das maiores e mais brilhantes estrelas da
           constelação de Corvus. Gigante do tipo espectral e inquieta assim como
-          nós. <br/> O mundo da criação mudou, e nós também estamos em crescente evolução, buscando
-          sempre as melhores soluções com as melhores ferramentas para nossos clientes.
-          Valorizamos os processos. Respeitamos identidades, criamos
-          autoridades.
+          nós. <br /> O mundo da criação mudou e nós também estamos em crescente
+          evolução, buscando sempre as melhores soluções com as melhores
+          ferramentas para nossos clientes. Valorizamos processos. Respeitamos
+          identidades. Criamos autoridades.
         </p>
         <br />
-        <p><strong>Kraz. Simples assim. Somos KRAZ.</strong></p>
+        <p>
+          <strong>Simples assim. Somos KRAZ.</strong>
+        </p>
       </Styled.LeftSection>
       <Styled.RightSection></Styled.RightSection>
     </Styled.Main>
